808a619cacf80f8dba41aaa41de6fcb0f00992a1
Mint <rwb0000@gmail.com>
2015-12-14 13:13:17 +0000

    machine learning classifier SVM to be applied on stream (pipeline)

---
+++ supermarket_scraper/search_loader.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb  6 11:03:33 2014
+
+@author: onsbigdata
+"""
+
+import csv
+import os
+
+class SearchLoader():
+    name = "SearchLoader"
+    data_path = ""
+    ml_path = ""
+
+    def __init__(self, *args, **kwargs):
+        thisloc = os.path.dirname(__file__)
+        self.data_path =  os.path.join(thisloc,"..","..","input")
+        self.ml_path = os.path.join(thisloc,"ml_model")
+
+
+    def get_file_reader(self,fname):
+        floc = os.path.join(self.data_path,fname)
+        fp = open(floc,'rb')
+        return csv.DictReader(fp, dialect='excel', delimiter=',')
+
+    def get_url_queries(self,fname):
+        rdr = self.get_file_reader(fname)
+        queries = []
+        for row in rdr:
+            queries.append(row)
+        return queries
+
+    #to access the machine learning parameter estimates (RB)
+    def generic_opener(self,fname):
+        floc = os.path.join(self.ml_path,fname)
+        fp = open(floc,'rb')
+        return fp 
+
+        
+        
